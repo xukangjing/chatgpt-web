01ec8b9565c6e308948170dee1afeb70d045a479
徐康景 <xukangjing@xiaomi.com>
2023-07-18 20:22:46 +0800

    解决本地ts文件报红；修改头像

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,8 +15,10 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
+      // avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
+      avatar: 'https://thumbnail1.baidupcs.com/thumbnail/19a2b99b1n40efa4236384b42cf795bc?fid=2937758048-250528-90269084088841&rt=pr&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-YykDhzA2fSXWVVw0NtBBFrgTPoE%3d&expires=8h&chkbd=0&chkv=0&dp-logid=9201037128381954061&dp-callid=0&time=1689598800&size=c1536_u864&quality=90&vuk=2937758048&ft=image&autopolicy=1',
+      // name: 'ChenZhaoYu',
+      name: 'xiaoba',
       description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
